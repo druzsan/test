eb802762b24d78955cbc9fd7b52a51065958ea9e
Alexander Druz <alexandersdruz@gmail.com>
2018-11-25 09:08:11 +0100

    commit 5

--- test.py
+++ test.py
@@ -1,11 +1,16 @@
 import cv2
-# from .models.vgg16 import VGG16 as net
-# from .models.imagenet_utils import preprocess_input, decode_predictions
+from .models.vgg16 import VGG16 as net
+from .models.imagenet_utils import preprocess_input, decode_predictions
 
-# model = net()
+model = net()
 
 image = cv2.imread("./data/Aaron_Peirsol_0001.jpg")
 # cv2.imshow("image", image)
 image = cv2.resize(image, (224, 224))
 # cv2.imshow("resized image", image)
 # cv2.waitKey(0)
+
+input = preprocess_input(image)
+output = model.predict(input)
+prediction = decode_predictions(output)
+print(prediction)
