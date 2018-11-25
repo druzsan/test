93290c197c3adf50b567fdd70fc72f366a3a0f64
Alexander Druz <alexandersdruz@gmail.com>
2018-11-25 09:05:46 +0100

    commit 4

--- test.py
+++ test.py
@@ -2,6 +2,10 @@ import cv2
 # from .models.vgg16 import VGG16 as net
 # from .models.imagenet_utils import preprocess_input, decode_predictions
 
+# model = net()
+
 image = cv2.imread("./data/Aaron_Peirsol_0001.jpg")
-cv2.imshow("image", image)
+# cv2.imshow("image", image)
+image = cv2.resize(image, (224, 224))
+# cv2.imshow("resized image", image)
 # cv2.waitKey(0)
