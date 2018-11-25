ab5f46e8ba54bf941d9b3308fd968e5b8d20edd8
Alexander Druz <alexandersdruz@gmail.com>
2018-11-25 08:59:08 +0100

    commit 2

--- test.py
+++ test.py
@@ -1,1 +1,7 @@
-print("I\'m ready for using")
+import cv2
+# from .models.vgg16 import VGG16 as net
+# from .models.imagenet_utils import preprocess_input, decode_predictions
+
+image = cv2.imread("./data/Aaron_Peirsol_0001.jpg")
+cv2.imshow("image", image)
+cv2.waitKey(0)
